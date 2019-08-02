5fdcb664d0bf4c616b93cde684a232ef31972e71
SETO(JSD) <SETO(JSD)@SETOJSD-HP>
2019-08-02 12:56:13 +0900

    仮作成

---
+++ test_0613f/src/servlet2/SearchServlet.java
@@ -0,0 +1,50 @@
+package servlet2;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/Search")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//
+		// 仮作成。実装はこれから。
+		// 内部処理のイメージとしては、
+		// パラメータ受け取り → DAOで検索 → 結果受け取り → 結果を元に情報をセット
+		// となるだろうか。
+		//
+
+		RequestDispatcher dispatch = request.getRequestDispatcher("/registerResult.jsp");
+		dispatch.forward(request, response);
+	}
+
+}
