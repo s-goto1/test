894fc0b1adeb74a19fc373e2655ac97b2e07e64d
SETO(JSD) <SETO(JSD)@SETOJSD-HP>
2019-08-16 10:54:27 +0900

    管理者対応

---
+++ test_0613f/WebContent/vacation/homeAdmin.jsp
@@ -0,0 +1,191 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>休暇申請（全社員）</title>
+<link rel="stylesheet" type="text/css"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" />
+<script type="text/javascript"
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript"
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script type="text/javascript"
+	src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../js/subtest.js"></script>
+</head>
+<body>
+	<div class="container col-md-10 col-md-offset-2 mt-4 mb-4">
+
+		<form id="date" name="date" action="Date" method="post">
+
+			<%@ include file="../year.jsp"%>
+
+			<input type="submit" id="Test" class="btn btn-info ml-2"
+				value="該当年表示">
+
+		</form>
+
+		<button type="button" class="btn btn-success mt-3" data-toggle="collapse"
+			data-target=".multi-collapse" aria-expanded="false">全開閉</button>
+
+		<form id="form" name="form" action="" method="post">
+			<p class="mt-3">
+				<b>全社員</b>の休暇申請一覧データ
+				（<c:out value="${year}年分" />）
+			</p>
+
+			<c:if test="${not empty map}">
+				<c:forEach var="i" items="${map}" varStatus="status">
+					<div class="accordion mb-4" id="accordion" role="tablist">
+						<div class="card">
+							<div class="card-header" id="header-${status.count}" role="tab">
+								<button class="btn btn-link btn-block text-left" type="button"
+									data-toggle="collapse" data-target="#card-${status.count}"
+										aria-expanded="false"
+									aria-controls="card-${status.count}">
+									<b><c:out value="${nameList.get(status.index)}さん" /></b>
+								</button>
+							</div>
+							<div id="card-${status.count}" class="collapse show multi-collapse" role="tabpanel"
+								aria-labelledby="header-${status.count}">
+								<div class="card-body">
+									<table border="1" class="table table-striped" style="border: solid 3px;">
+										<tbody style="border: black 2px">
+											<tr>
+												<th colspan="3">
+													<div class="text-center">期間</div>
+												</th>
+
+												<th>
+													<div class="text-center">区分</div>
+												</th>
+
+												<th>
+													<div class="text-center">事由</div>
+												</th>
+											</tr>
+
+											<c:forEach var="item" items="${i.value}" begin="0" end="${i.value.size()}">
+												<tr>
+													<td style="border-right: hidden;"><c:if test="${not empty map}">
+														<c:out value="${item.fromMonth}月" />
+														<c:out value="${item.fromDay}日より" />
+													</c:if></td>
+
+												<td style="border-right: hidden;"><c:if test="${not empty map}">
+														<c:out value="${item.toMonth}月" />
+														<c:out value="${item.toDay}日まで" />
+													</c:if></td>
+
+												<td><c:if test="${not empty map}">
+														<c:out value="(${item.totalDay}日間)" />
+													</c:if></td>
+
+												<td><c:if test="${not empty map}">
+														<c:out value="${item.division}" />
+													</c:if></td>
+
+												<td><c:if test="${not empty map}">
+														<c:out value="${item.reason}" />
+													</c:if></td>
+												</tr>
+											</c:forEach>
+
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</c:forEach>
+			</c:if>
+
+			<c:if test="${map.isEmpty()}">
+				<p>
+					<c:out value=" ${nomap}" />
+				</p>
+			</c:if>
+
+			<c:if test="${not empty map}">
+				<nav class="mb-4" aria-label="Page navigation">
+					<ul class="pagination justify-content-center">
+						<c:choose>
+							<c:when test="${currentpage == 1}">
+								<li class="page-item disabled">
+									<a class="page-link" href="#">
+										Prev
+									</a>
+								</li>
+							</c:when>
+							<c:otherwise>
+								<li class="page-item">
+									<a class="page-link"
+										href="PagingServTest?page=${currentpage - 1}">
+										Prev
+									</a>
+								</li>
+							</c:otherwise>
+						</c:choose>
+
+						<c:forEach begin="1" end="${number}" step="1" varStatus="status">
+							<c:choose>
+								<c:when test="${currentpage == status.index}">
+									<li class="page-item active">
+										<span class="page-link">
+											${status.index}
+											<span class="sr-only">
+												(current)
+											</span>
+										</span>
+									</li>
+								</c:when>
+								<c:otherwise>
+									<li class="page-item">
+										<a class="page-link"
+											href="PagingServTest?page=${status.index}">
+											${status.index}
+										</a>
+									</li>
+								</c:otherwise>
+							</c:choose>
+						</c:forEach>
+
+						<c:choose>
+							<c:when test="${currentpage == number}">
+								<li class="page-item disabled">
+									<a class="page-link" href="#">
+										Next
+									</a>
+								</li>
+							</c:when>
+							<c:otherwise>
+								<li class="page-item">
+									<a class="page-link"
+										href="PagingServTest?page=${currentpage + 1}">
+										Next
+									</a>
+								</li>
+							</c:otherwise>
+						</c:choose>
+					</ul>
+				</nav>
+			</c:if>
+
+
+			<!--<input type="button" id="excelout"
+				class="btn btn-warning mr-2" value="Excelに出力">-->
+			<input type="button" id="admin"
+				class="btn btn-primary mr-2" value="管理画面">
+			<input type="button" id="menu" class="btn btn-light mr-2"
+				onclick="location.href='../menu.jsp'" value="メニュー">
+			<input type="button" id="logout" class="btn btn-secondary"
+				onclick="location.href='../logout.jsp'" value="ログアウト">
+
+		</form>
+
+	</div>
+</body>
+</html>
---
+++ test_0613f/src/servlet2/vacation/SearchServlet.java
@@ -0,0 +1,303 @@
+package servlet2.vacation;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.lucene.search.spell.LevenshteinDistance;
+
+import entity.Vacation;
+import test_0613f.LoginUser;
+import test_0613f.business.SearchDao;
+import test_0613f.vacation.VacationDao;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/vacation/Search")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 文字化け回避
+		request.setCharacterEncoding("UTF-8");
+
+		// セッション取得の準備
+		HttpSession session = request.getSession();
+
+		// セッションから情報を取得
+		//String masterId = (String) session.getAttribute("masterId");
+		String masterName = (String) session.getAttribute("masterName");
+		Integer year = (Integer) session.getAttribute("year");
+		Integer fromMonth = (Integer) session.getAttribute("fromMonth");
+
+		// 「ID検索」が押された？
+		if(request.getParameter("idSearch") != null) {
+			// 入力情報の取得
+			final String id = request.getParameter("id");
+
+			// DAOの宣言
+			SearchDao search = new SearchDao();
+			VacationDao vacation = new VacationDao();
+			LoginUser user = new LoginUser();
+
+			// 入力フォームが空欄？
+			if(id == null || id.equals("")) {
+				// 今月の出張清算データがある人物のIDを取得
+				List<String> idList = search.findIdDistinct(year, fromMonth);
+
+				// 今月の出張清算データがある人物の名前を取得
+				List<String> nameList = search.findNameDistinct(year, fromMonth);
+
+				// データが1件でもある？
+				if(idList.size() > 0 && nameList.size() > 0) {
+					// 変数宣言
+					int index = 0;
+
+					// リストのサイズを取得
+					int size = idList.size();
+
+					// ページ数を取得
+					int number = (size + 5 - 1) / 5;
+
+					// 5人以上いる？
+					if(size >= 5) {
+						// toIndexの値を5に固定
+						index = 5;
+					// 5人未満しかいない？
+					} else {
+						// toIndexの値を人数でセット
+						index = size;
+					}
+
+					// 1度に表示するユーザ数を制限
+					List<String> idListLimit = idList.subList(0, index);
+
+					// 今月分の規定数の社員の出張精算データを取得
+					Map<String, List<Vacation>> map = idListLimit.stream()
+							.collect(Collectors.toMap(
+									s -> s,
+									s -> vacation.findAllByMonthForIdFromAdmin(s, year, fromMonth)));
+
+					// セッションに情報をセット
+					session.setAttribute("currentpage", 1);
+					session.setAttribute("size", size);
+					session.setAttribute("number", number);
+					session.setAttribute("map", map);
+					session.setAttribute("idList", idList);
+					session.setAttribute("nameList", nameList);
+				// データが1件もない？
+				} else {
+					// セッションに情報をセット
+					session.setAttribute("nomap", "登録されているデータがありません。");
+				}
+
+				// homeAdmin.jspに遷移
+				RequestDispatcher dispatch = request.getRequestDispatcher("/vacation/homeAdmin.jsp");
+				dispatch.forward(request, response);
+			// 入力フォームに入力あり？
+			} else {
+				// 自分以外の全ユーザのIDを取得
+				List<String> userList = search.findIdAll(masterName);
+
+				// レーベンシュタイン距離による評価関数
+				LevenshteinDistance dis =  new LevenshteinDistance();
+				ToIntFunction<String> dist = s -> {
+					return (int) (dis.getDistance(id, s) * 100);
+					};
+
+				// 該当のIDか近しいIDを取得
+				String closest = userList.stream()
+						.max(Comparator.comparingInt(dist))
+						.orElse(id);
+
+				// レーベンシュタイン距離が離れすぎている？
+				if(dis.getDistance(id, closest) * 100 < 40) {
+					// リクエストに情報をセット
+					request.setAttribute("error", "近しいIDが存在しません。文字数が少ない場合にはもう少し長く、"
+							+ "文字数が多い場合にはもう少し短くして再度検索してみて下さい。"
+							+ "あるいは、もう少し具体的な内容の文字列で検索してみて下さい。");
+
+					// search.jspに遷移
+					RequestDispatcher dispatch = request.getRequestDispatcher("/business/search.jsp");
+					dispatch.forward(request, response);
+				// レーベンシュタイン距離は適切な長さ？
+				} else {
+					// ヒットしたユーザの情報を取得
+					List<Vacation> list = vacation.findAllByMonthForId(closest, year, fromMonth, 0);
+
+					// ヒットしたユーザの情報を元にそのユーザの名前を取得
+					String name = user.findUser(closest).getName();
+
+					// ページングなしでのレコード数取得
+					int count = vacation.countRow(closest, year, fromMonth);
+
+					// ページング設定
+					int number = (count + 5 - 1) / 5;
+
+					// リストのサイズを取得
+					//int size = list.size();
+
+					// ヒットしたユーザの情報をセッションにセット
+					session.setAttribute("id", closest);
+					session.setAttribute("name", name);
+					session.setAttribute("currentpage", 1);
+					session.setAttribute("number", number);
+					session.setAttribute("list", list);
+
+					// home.jspに遷移
+					RequestDispatcher dispatch = request.getRequestDispatcher("/vacation/home.jsp");
+					dispatch.forward(request, response);
+				}
+			}
+		// 「名前検索」が押された？
+		} else if(request.getParameter("nameSearch") != null) {
+			// 入力情報の取得
+			final String name = request.getParameter("name");
+
+			// DAOの宣言
+			SearchDao search = new SearchDao();
+			VacationDao vacation = new VacationDao();
+
+			// 入力フォームが空欄？
+			if(name == null || name.equals("")) {
+				// 今月の出張清算データがある人物のIDを取得
+				List<String> idList = search.findIdDistinct(year, fromMonth);
+
+				// 今月の出張清算データがある人物の名前を取得
+				List<String> nameList = search.findNameDistinct(year, fromMonth);
+
+				// データが1件でもある？
+				if(idList.size() > 0 && nameList.size() > 0) {
+					// 変数宣言
+					int index = 0;
+
+					// リストのサイズを取得
+					int size = nameList.size();
+
+					// ページ数を取得
+					int number = (size + 5 - 1) / 5;
+
+					// 5人以上いる？
+					if(size >= 5) {
+						// toIndexの値を5に固定
+						index = 5;
+					// 5人以下未満しかいない？
+					} else {
+						// toIndexの値を人数でセット
+						index = size;
+					}
+
+					// 1度に表示するユーザ数を制限
+					List<String> nameListLimit = nameList.subList(0, index);
+
+					// 今月分の規定数の社員の出張精算データを取得
+					Map<String, List<Vacation>> map = nameListLimit.stream()
+							.collect(Collectors.toMap(
+									s -> s,
+									s -> vacation.findAllByMonthForNameFromAdmin(s, year, fromMonth)));
+
+					// セッションに情報をセット
+					session.setAttribute("currentpage", 1);
+					session.setAttribute("size", size);
+					session.setAttribute("number", number);
+					session.setAttribute("map", map);
+					session.setAttribute("idList", idList);
+					session.setAttribute("nameList", nameList);
+				// データが1件もない？
+				} else {
+					// セッションに情報をセット
+					session.setAttribute("nomap", "登録されているデータがありません。");
+				}
+
+				// homeAdmin.jspに遷移
+				RequestDispatcher dispatch = request.getRequestDispatcher("/vacation/homeAdmin.jsp");
+				dispatch.forward(request, response);
+			// 入力フォームに入力あり？
+			} else {
+				// 自分以外の全ユーザの名前を取得
+				List<String> userList = search.findNameAll();
+
+				// レーベンシュタイン距離による評価関数
+				LevenshteinDistance dis =  new LevenshteinDistance();
+				ToIntFunction<String> dist = s -> {
+					return (int) (dis.getDistance(name, s) * 100);
+					};
+
+				// 該当の名前か近しい名前を取得
+				String closest = userList.stream()
+						.max(Comparator.comparingInt(dist))
+						.orElse(name);
+
+				// レーベンシュタイン距離が離れすぎている？
+				if(dis.getDistance(name, closest) * 100 < 40) {
+					// リクエストに情報をセット
+					request.setAttribute("error", "近しい名前が存在しません。文字数が少ない場合にはもう少し長く、"
+							+ "文字数が多い場合にはもう少し短くして再度検索してみて下さい。"
+							+ "あるいは、もう少し具体的な内容の文字列で検索してみて下さい。");
+
+					// search.jspに遷移
+					RequestDispatcher dispatch = request.getRequestDispatcher("/vacation/search.jsp");
+					dispatch.forward(request, response);
+				// レーベンシュタイン距離は適切な長さ？
+				} else {
+					// ヒットしたユーザの情報を取得
+					List<Vacation> list = vacation.findAllByMonthForName(closest, year, fromMonth, 0);
+
+					// ヒットしたユーザの情報を元にそのユーザのIDを取得
+					String id = list.get(0).getId();
+
+					// ページングなしでのレコード数取得
+					int count = vacation.countRow(id, year, fromMonth);
+
+					// ページング設定
+					int number = (count + 5 - 1) / 5;
+
+					// リストのサイズを取得
+					//int size = list.size();
+
+					// ヒットしたユーザの情報をセッションにセット
+					session.setAttribute("id", id);
+					session.setAttribute("name", closest);
+					session.setAttribute("currentpage", 1);
+					session.setAttribute("number", number);
+					session.setAttribute("list", list);
+
+					// home.jspに遷移
+					RequestDispatcher dispatch = request.getRequestDispatcher("/vacation/home.jsp");
+					dispatch.forward(request, response);
+				}
+			}
+		}
+	}
+}
